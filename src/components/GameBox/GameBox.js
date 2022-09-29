4ebbc9751fffa2bea77c3936f758d52695cfd15c
Antoniue <Verstiplayingtm@gmail.com>
2022-09-29 15:02:09 +0300

    added GameBox folder for that specific component, downloaded images and changed import

---
+++ src/components/GameBox/GameBox.js
@@ -0,0 +1,26 @@
+import './GameBox.css'
+function GameBox(){
+    return(
+        <div className="box">
+            <div className="scoresBox">
+                <ul className="scores">
+                    <li className="historicalBestScore">Historical Best: </li>
+                    <li className="currentScore">Score: </li>
+                    <li className="currentBestScore">Session Best: </li>
+                </ul>
+            </div>
+            <div className="cardsBox">
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+            </div>
+        </div>
+    );
+}
+
+export {GameBox};
