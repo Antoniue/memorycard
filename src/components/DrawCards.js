67265d5004c7ea1d15002f4499a204ee322a7dfa
Antoniue <Verstiplayingtm@gmail.com>
2022-09-30 09:00:48 +0300

    project finished, now checks if an img src has been clicked twice, updates score accordingly

--- src/components/DrawCards.js
+++ src/components/DrawCards.js
@@ -1,14 +1,41 @@
-function DrawCards({images}){
+import {useState} from "react";
+
+function DrawCards({setScore,setBestScore,setHistoricalScore,images, score, bestScore, historicalScore}){
+    const [picked, setPicked] = useState([]);
+    
+    localStorage.setItem('score', historicalScore);
+
+    function logi(card){
+        if(picked.includes(card.src))
+        {
+            console.log(card.src);
+            console.log('includes');
+            setPicked([]);
+            if(score >= bestScore)
+                setBestScore(score);
+            setScore(0);
+            if(score >= historicalScore)
+                setHistoricalScore(score);
+        }
+        else if (!picked.includes(card.src))
+        {
+            console.log(card.src);
+            console.log('doesnt');
+            setPicked(prev => [...prev, card.src]);
+            setScore(prev => prev+1);
+            console.log(score);
+        }
+    }
     return(
         <>
-            <div className="card"><img src={images[0]} alt="" /></div>
-            <div className="card"><img src={images[1]} alt="" /></div>
-            <div className="card"><img src={images[2]} alt="" /></div>
-            <div className="card"><img src={images[3]} alt="" /></div>
-            <div className="card"><img src={images[4]} alt="" /></div>
-            <div className="card"><img src={images[5]} alt="" /></div>
-            <div className="card"><img src={images[6]} alt="" /></div>
-            <div className="card"><img src={images[7]} alt="" /></div>
+            <div className="card" onClick={(card) => {logi(card.target)}}><img src={images[0]} alt="" /></div>
+            <div className="card" onClick={(card) => {logi(card.target)}}><img src={images[1]} alt="" /></div>
+            <div className="card" onClick={(card) => {logi(card.target)}}><img src={images[2]} alt="" /></div>
+            <div className="card" onClick={(card) => {logi(card.target)}}><img src={images[3]} alt="" /></div>
+            <div className="card" onClick={(card) => {logi(card.target)}}><img src={images[4]} alt="" /></div>
+            <div className="card" onClick={(card) => {logi(card.target)}}><img src={images[5]} alt="" /></div>
+            <div className="card" onClick={(card) => {logi(card.target)}}><img src={images[6]} alt="" /></div>
+            <div className="card" onClick={(card) => {logi(card.target)}}><img src={images[7]} alt="" /></div>
         </>
     );
 }
--- src/components/GameBox/GameBox.js
+++ src/components/GameBox/GameBox.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {useEffect, useState } from 'react';
 import { DrawCards } from '../DrawCards';
 import './GameBox.css';
 import poke1 from './images/poke1.png';
@@ -10,32 +10,35 @@ import poke6 from './images/poke6.png';
 import poke7 from './images/poke7.png';
 import poke8 from './images/poke8.png';
 
+function shuffleCards(cards){
+    for(let index = 0; index < 8; index++)
+    {
+        let num = Math.floor(Math.random() * 8);
+        [cards[num], cards[index]] = [cards[index],cards[num]];
+    }
+    return cards;
+};
+
 function GameBox(){
     const [images, setImages] = useState([poke1,poke2,poke3,poke4,poke5,poke6,poke7,poke8]);
+    const [score, setScore] = useState(0);
+    const [bestScore, setBestScore] = useState(0);
+    const [historicalScore, setHistoricalScore] = useState(Number(localStorage.getItem('score')));
 
-    function shuffleCards(cards){
-        for(let index = 0; index < 8; index++)
-        {
-            let num = Math.floor(Math.random() * 8);
-            [cards[num], cards[index]] = [cards[index],cards[num]];
-        }
-        return cards;
-    };
-    
     return(
         <div className="box">
             <div className="scoresBox">
                 <ul className="scores">
-                    <li className="historicalBestScore">Historical Best: </li>
-                    <li className="currentScore">Score: </li>
-                    <li className="currentBestScore">Session Best: </li>
+                    <li className="historicalBestScore">Historical Best: {historicalScore}</li>
+                    <li className="currentScore">Score: {score}</li>
+                    <li className="currentBestScore">Session Best: {bestScore}</li>
                 </ul>
             </div>
             <div className="cardsBox">
-                <DrawCards images={shuffleCards(images)}/>
+                <DrawCards images={shuffleCards(images)} setScore={setScore} setBestScore={setBestScore} setHistoricalScore={setHistoricalScore} score={score} bestScore={bestScore} historicalScore={historicalScore}/>
             </div>
         </div>
     );
 }
 
-export {GameBox};
+export {GameBox, shuffleCards};
